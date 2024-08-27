02f18991ffdd421732ba5a89e4d1b243a6012485
thureindev <thureindev@outlook.com>
2024-08-27 14:41:02 +0700

    router okay

--- src/router.js
+++ src/router.js
@@ -4,20 +4,14 @@ import HomeView from './components/HomeView.vue';
 import AboutView from './components/AboutView.vue';
 
 const navRoutes = [
-    { name: 'Dashboard', href: '#', current: true },
-    { name: 'Team', href: '#', current: false },
-    { name: 'Projects', href: '#', current: false },
-    { name: 'Calendar', href: '#', current: false },
-    { name: 'Reports', href: '#', current: false },
+    { name: 'Dashboard', href: '/', current: true, component: HomeView },
+    { name: 'Team', href: '/about', current: false, component: AboutView },
+    { name: 'Projects', href: '/', current: false, component: HomeView },
+    { name: 'Calendar', href: '/about', current: false, component: AboutView },
+    { name: 'Reports', href: '/', current: false, component: HomeView },
 ]
 
-const routes = navRoutes.map(item => {path: item.href})
-
-[
-    { path: '/', component: HomeView },
-    { path: '/about', component: AboutView },
-]
-
+const routes = navRoutes.map((item) => ({ path: item.href, component: item.component }));
 const router = createRouter({
     history: createMemoryHistory(),
     routes,
