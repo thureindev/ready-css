da12fd95bf1610da13f0ac81a464ca40fdcc21d9
thureindev <thureindev@outlook.com>
2024-08-30 15:18:47 +0700

    github pages deploy base url change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/thureindev/ready-css",
+  base: "/ready-css/",
   plugins: [vue()],
 })
